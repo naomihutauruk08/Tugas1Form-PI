9429229bd82fd2693fbe641c81aacf13fb2df83f
putrimalik <putrihandayaniparinduri@gmail.com>
2021-04-03 21:06:42 +0700

    Mengisi file connection database

--- crud.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("connection_db.php");
-
-if(isset($_GET['id']) && isset($_GET['action'])) {
-  if($_GET['action'] == 'delete') {
-    deleteWhere('data_mahasiswa','id',$_GET['id']);
-  }
-}
